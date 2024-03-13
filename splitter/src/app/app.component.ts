78d53d215749556bcb1de807b2c32025e3ae0692
Yannic Mair <yannic.mair@nexus-ag.de>
2024-03-13 09:56:02 +0100

    first correct implementation of videoPlayer

--- splitter/src/app/app.component.ts
+++ splitter/src/app/app.component.ts
@@ -10,13 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent
 {
-    title = 'splitter';
-
     @ViewChild('videoPlayer') videoPlayer!: ElementRef;
-    isPlaying: boolean = false;
+    public isPlaying: boolean = false;
+    public isFullScreen: boolean = false;
+    public volume: number = 1;
 
-    getButtonText = () => this.isPlaying ? 'Pause' : 'Weiter';
-    
     togglePlayPause(): void
     {
         if (this.videoPlayer.nativeElement.paused)
@@ -28,5 +26,57 @@ export class AppComponent
             this.videoPlayer.nativeElement.pause();
             this.isPlaying = false;
         }
+    }
+
+    toggleFullScreen(): void
+    {
+        const elem = this.videoPlayer.nativeElement as any;
+        if (!this.isFullScreen)
+        {
+            if (elem.requestFullscreen)
+            {
+                elem.requestFullscreen();
+            }
+            else if (elem.mozRequestFullScreen)
+            {
+                elem.mozRequestFullScreen();
+            }
+            else if (elem.webkitRequestFullscreen)
+            {
+                elem.webkitRequestFullscreen();
+            }
+            else if (elem.msRequestFullscreen)
+            {
+                elem.msRequestFullscreen();
+            }
+        }
+
+        this.isFullScreen = !this.isFullScreen;
+    }
+
+    adjustVolume(direction: string): void
+    {
+        if (direction === 'up')
+        {
+            if (this.volume < 1)
+            {
+                this.volume += 0.1;
+            }
+        } else if (direction === 'down')
+        {
+            if (this.volume > 0)
+            {
+                this.volume -= 0.1;
+            }
+        }
+        this.videoPlayer.nativeElement.volume = this.volume;
+    }
+
+    skipVideo(seconds: number) 
+    {
+        const currentTime = this.videoPlayer.nativeElement.currentTime;
+        let newTime = currentTime + seconds;
+        newTime = Math.max(0, Math.min(this.videoPlayer.nativeElement.duration, newTime));
+        this.videoPlayer.nativeElement.currentTime = newTime;
     }
 }
